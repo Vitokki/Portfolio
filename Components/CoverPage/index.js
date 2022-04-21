26c4e3b83510c0a30cafd7ab805c797deea17612
Vitokki <thedaverush@hotmail.com>
2022-04-21 01:24:45 -0500

    Cover Page

---
+++ Components/CoverPage/index.js
@@ -0,0 +1,11 @@
+import styles from "../../styles/CoverPage.module.css";
+
+const CoverPage = () => {
+  return (
+    <div
+      className={styles.container}
+    ></div>
+  );
+};
+
+export default CoverPage;
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import CoverPage from '../Components/CoverPage/index';
+
 export default function Home() {
   return (
     <div className={styles.container}>
+      <CoverPage></CoverPage>
+      <CoverPage></CoverPage>
     </div>
   )
 }
